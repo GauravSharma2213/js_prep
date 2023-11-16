46d11327ba34d2ac2b6ad5a7d0744b8cb7d31775
Gaurav Sharma <133696890+GauravSharma2213@users.noreply.github.com>
2023-11-16 23:24:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3 + 3) we are using nodeJS, not browser
+
+console.log(1+3)
+console.log("Gaurav")
+// code readability should be the priortiy. the code above can be written in a single line by using ; but is should be avoided to improve the code readability.
+
+let name = "Gaurav" 
+// String => ""
+
+let age = 22
+// numbers => 2 to power 53
+
+let isLoggedIn = true
+// boolian => true/false
+
+// bigint is used for very big numbers
+
+// null => satndalone value, ths is repersentation of an empty value (intentionally assigned to be zero)
+// undefined => when you have not defined a value to the variable
+
+// symbol => unique values
+
+// object
+
+console.log(typeof "gaurav");
+// This command is used to determine the datatypes of variables

6a307c5ab317af15433a0ecd2c029dc727c3895e
Gaurav.Sharma <gouravsharma2513@gmail.com>
2023-12-11 17:18:43 -0500

    Switch Statement syntax

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,54 @@
+//This is the basic syntax of a switch statement
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 5
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+
+//     default:
+//         console.log("Defalut case");
+//         break;
+// }
+
+// In case of String:
+
+const month = "Jan"
+
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "Mar":
+        console.log("Mar");
+        break;
+    case "Apr":
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Defalut case");
+        break;
+}

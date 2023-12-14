34a27f2729247a14bbd81050aeda4a52ff3f5df4
Gaurav.Sharma <gouravsharma2513@gmail.com>
2023-12-13 23:55:48 -0500

    loops basics

---
+++ 05_iterations/one.js
@@ -0,0 +1,25 @@
+// For loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// Firstly, we took a variable: index. Then we do the condition check with the array lenght property and lastly
+// we increace the varibale named index. After that in the block scope we print the array's value
+
+// Line by line working of a loop:(line no.3) You take a varibale: index then do the condition check > directly after that you 
+// print that value (line no 4) > Then it goes back and add another value to the variable (line number 3)
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+
+        //console.log("Bas karde yaar print kuch v"); //***/
+        
+    }
+    // console.log(element);
+}
+// In this loop the line with starts peinted in the middle because its supposed to go to the last line before going up
+// and adding a number to it
+

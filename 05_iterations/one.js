0985acbb936458df98b82877c32c6dc529a99029
Gaurav.Sharma <gouravsharma2513@gmail.com>
2023-12-14 16:22:09 -0500

    continue and break

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -23,3 +23,52 @@ for (let i = 0; i < 10; i++) {
 // In this loop the line with starts peinted in the middle because its supposed to go to the last line before going up
 // and adding a number to it
 
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        //console.log(`Inner loop value: ${j} and outer loop ${i}`);
+        // console.log(i + '*' + j + '=' + i*j);        
+    }
+    
+}
+
+let myArray = ["Flash", "Batman", "Superman"]
+
+//console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+
+    //console.log(element);
+    
+}
+
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+
+// Here the loop isn't printing upto 20 because of break. It makes the jump directly out of this scope.
+// Hence prevents it from going back up again and printing next. 
+
+//     console.log(`Value of i is ${index}`);
+    
+// }
+
+// Continue: 
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+
+// Continue lets your code ignore the condition once and keeps on running the loop. It does not makes you exit the scope like break.
+
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
